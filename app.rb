62f1ddcb17b26efb3edc6437bf3f61174a47f09e
Ernest Fraiman <upapaus@gmail.com>
2018-11-09 11:59:04 +0000

    added the ability to create new developer on website and saved it into db

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 
+require_relative( 'controllers/developers_controller')
+
 get '/' do
   erb( :index )
 end
---
+++ controllers/developers_controller.rb
@@ -0,0 +1,23 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/developer.rb' )
+also_reload( '../models/*' )
+
+get '/developers' do
+  @developers = Developer.all()
+  erb ( :"developers/index" )
+end
+
+get '/developers/new' do
+  erb(:"developers/new")
+end
+
+post '/developers' do
+  Developer.new(params).save
+  redirect to '/developers'
+end
+
+get '/developers/:id' do
+  @developer = Developer.find(params['id'].to_i)
+  erb( :"developers/show" )
+end
