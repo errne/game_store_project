a08e2ed0c170e4e371d4bb3e6ad80355a3861638
Ernest Fraiman <upapaus@gmail.com>
2018-11-12 17:03:31 +0000

    added views for tag.class

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require( 'sinatra/contrib/all' )
 require_relative( 'controllers/developers_controller')
 require_relative( 'controllers/publishers_controller')
 require_relative( 'controllers/games_controller')
+require_relative( 'controllers/tags_controller')
 
 get '/' do
   erb( :index )
